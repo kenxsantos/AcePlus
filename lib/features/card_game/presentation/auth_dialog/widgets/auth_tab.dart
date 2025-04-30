47695a3452e474b2a6a9eababb45d6e57fb8541d
kenxsantos <kenxsantos@gmail.com>
2025-04-30 17:50:55 +0800

    feat: add authentication dialog with login and registration forms

---
+++ lib/features/card_game/presentation/auth_dialog/widgets/auth_tab.dart
@@ -0,0 +1,63 @@
+import 'package:aceplus/features/card_game/presentation/auth_dialog/widgets/login_form.dart';
+import 'package:aceplus/features/card_game/presentation/auth_dialog/widgets/register_form.dart';
+import 'package:aceplus/shared/utils/constant.dart';
+import 'package:aceplus/shared/utils/strings.dart';
+
+import 'package:flutter/material.dart';
+
+class AuthTabBar extends StatefulWidget {
+  const AuthTabBar({super.key});
+
+  @override
+  State<AuthTabBar> createState() => _AuthTabBarState();
+}
+
+class _AuthTabBarState extends State<AuthTabBar>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TabBar.secondary(
+          indicatorColor: Color(0xFFE7BB68),
+          indicatorWeight: 5,
+          dividerHeight: 5,
+          labelColor: primaryWhite,
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              child: Text(Str().logIn, style: TextStyle(fontFamily: poppins)),
+            ),
+            Tab(
+              child: Text(
+                Str().register,
+                style: TextStyle(fontFamily: poppins),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 300,
+          child: TabBarView(
+            controller: _tabController,
+            children: <Widget>[LoginForm(), RegisterForm()],
+          ),
+        ),
+      ],
+    );
+  }
+}
