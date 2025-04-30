4f938ba997f8826032541bf6a02fac218ad4db0c
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-04-30 16:51:19 +0800

    remove unused import

--- lib/features/card_game/presentation/wallet_page/widgets/history_tab.dart
+++ lib/features/card_game/presentation/wallet_page/widgets/history_tab.dart
@@ -1,4 +1,3 @@
-import 'package:aceplus/features/card_game/presentation/wallet_page/widgets/cash_in_container.dart';
 import 'package:aceplus/features/card_game/presentation/wallet_page/widgets/history_tile.dart';
 import 'package:aceplus/shared/utils/constant.dart';
 import 'package:aceplus/shared/utils/strings.dart';
