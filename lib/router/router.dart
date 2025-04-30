2bdba74342beb0b59df36603e43b9a7f53eaffa6
kenxsantos <kenxsantos@gmail.com>
2025-04-30 11:02:47 +0800

    feat: update routing to include receipt page and refactor transaction history route

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,6 +1,6 @@
-import 'package:aceplus/features/card_game/presentation/wallet_page/screens/transaction_history.dart';
+import 'package:aceplus/features/card_game/presentation/wallet_page/screens/receipt_page.dart';
+import 'package:aceplus/features/card_game/presentation/wallet_page/screens/transaction_history_page.dart';
 import 'package:aceplus/features/card_game/presentation/wallet_page/screens/wallet_page.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
@@ -10,41 +10,20 @@ class AppRouter {
     router = GoRouter(
       initialLocation: '/',
       routes: [
-        ShellRoute(
-          builder: (context, state, child) {
-            return Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.black,
-                automaticallyImplyLeading: true,
-                leading: IconButton(
-                  onPressed: () => {},
-                  color: Colors.white,
-                  icon: Icon(Icons.arrow_back_ios_new_rounded),
-                ),
-                actions: [
-                  IconButton(
-                    onPressed: () => context.goNamed("transaction-history"),
-                    icon: Icon(Icons.list),
-                    color: Colors.white,
-                    iconSize: 30,
-                  ),
-                ],
-              ),
-              body: child,
-            );
-          },
-          routes: [
-            GoRoute(
-              path: '/',
-              name: 'home',
-              builder: (context, state) => const WalletPage(),
-            ),
-            GoRoute(
-              path: '/transaction-history',
-              name: 'transaction-history',
-              builder: (context, state) => const TransactionHistory(),
-            ),
-          ],
+        GoRoute(
+          path: '/',
+          name: 'home',
+          builder: (context, state) => const WalletPage(),
+        ),
+        GoRoute(
+          path: '/transaction-history',
+          name: 'transaction-history',
+          builder: (context, state) => const TransactionHistoryPage(),
+        ),
+        GoRoute(
+          path: '/show-receipt',
+          name: 'show-receipt',
+          builder: (context, state) => const ReceiptPage(),
         ),
       ],
     );
