903cc071bac6d8bc9deeb84719727b9fa5ddb330
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-02 11:08:40 +0800

    update bottom toolbar

--- lib/features/card_game/presentation/game_page/game_widgets/bottom_bet_toolbar.dart
+++ lib/features/card_game/presentation/game_page/game_widgets/bottom_bet_toolbar.dart
@@ -1,82 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+          import 'package:google_fonts/google_fonts.dart';
 
-class BottomBetWidget extends StatelessWidget {
-  const BottomBetWidget({super.key});
+          class BottomToolbarWidget extends StatelessWidget {
+            final VoidCallback onTap;
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Image.asset(
-          'assets/images/bottom_placing_bet.png',
-          width: MediaQuery.of(context).size.width * 1,
-          height: 75,
-          fit: BoxFit.fill,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Stack(
-                children: [
-                  Text(
-                    'Place your bets !',
-                    style: GoogleFonts.lemon(
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        foreground:
-                            Paint()
-                              ..style = PaintingStyle.stroke
-                              ..strokeWidth = 1.5
-                              ..color = const Color(0xFF7D1F22),
+            const BottomToolbarWidget({super.key, required this.onTap});
+
+            @override
+            Widget build(BuildContext context) {
+              return GestureDetector(
+                onTap: onTap,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/bottom_placing_bet.png',
+                      width: MediaQuery.of(context).size.width * 1,
+                      height: 75,
+                      fit: BoxFit.fill,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width - 40,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Stack(
+                              children: [
+                                Text(
+                                  'Place your bets !',
+                                  style: GoogleFonts.lemon(
+                                    textStyle: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      foreground: Paint()
+                                        ..style = PaintingStyle.stroke
+                                        ..strokeWidth = 1.5
+                                        ..color = const Color(0xFF7D1F22),
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'Place your bets !',
+                                  style: GoogleFonts.lemon(
+                                    textStyle: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFFFFD700),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                // Add cancel button functionality here
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFFFFD700),
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50.0),
+                                  side: const BorderSide(
+                                    color: Color(0xFF7D1F22),
+                                    width: 1.5,
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                'Cancel',
+                                style: GoogleFonts.lemon(
+                                  textStyle: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    'Place your bets !',
-                    style: GoogleFonts.lemon(
-                      textStyle: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFFFFD700),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Add cancel button functionality here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFFFD700),
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                    side: const BorderSide(
-                      color: Color(0xFF7D1F22),
-                      width: 1.5,
-                    ),
-                  ),
+                  ],
                 ),
-                child: Text(
-                  'Cancel',
-                  style: GoogleFonts.lemon(
-                    textStyle: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
+              );
+            }
+          }
