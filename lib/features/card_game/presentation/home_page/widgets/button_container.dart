9538a7757fe9fcdaeefbf5c14e8af52e668b2f68
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-05 15:57:20 +0800

    apply logged in conditions if user is logged in

--- lib/features/card_game/presentation/home_page/widgets/button_container.dart
+++ lib/features/card_game/presentation/home_page/widgets/button_container.dart
@@ -2,27 +2,62 @@ import 'package:aceplus/shared/utils/strings.dart';
 import 'package:aceplus/shared/widgets/gradient_button.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../auth_dialog/screens/auth_dialog.dart';
 
 class ButtonContainer extends StatelessWidget {
   const ButtonContainer({super.key});
 
+  Future<bool> _isLoggedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('isLoggedIn') ?? false;
+  }
+
+  Future<String?> _getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getInt('userId');
+    return userId?.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 50),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          GradientButton(
-            labelText: Str().playNow,
-            onPressed: () => context.go('/game'),
+    return FutureBuilder<bool>(
+      future: _isLoggedIn(),
+      builder: (context, snapshot) {
+        final isLoggedIn = snapshot.data ?? false;
+
+        return Container(
+          margin: EdgeInsets.only(top: 50),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GradientButton(
+                labelText: Str().playNow,
+                onPressed: () async {
+                  if (isLoggedIn) {
+                    final userId = await _getUserId();
+                    print(userId);
+                    if(context.mounted) {
+                      context.go('/game/$userId');
+                    }
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AuthDialog(),
+                    );
+                  }
+                },
+              ),
+              if (isLoggedIn)
+                GradientButton(
+                  labelText: Str().wallet,
+                  onPressed: () => context.goNamed("wallet"),
+                ),
+            ],
           ),
-          GradientButton(
-            labelText: Str().wallet,
-            onPressed: () => context.goNamed("wallet"),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
