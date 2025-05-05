32f2333ca06e10acc2d8e7bb96d629a583206930
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-05 10:16:53 +0800

    add data source for Auth

---
+++ lib/core/data_source/data_source.dart
@@ -0,0 +1,25 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import '../model/auth_model/auth_model.dart';
+
+
+class AuthDataSource {
+  final Box<Auth> _authBox = Hive.box<Auth>('auth');
+
+  Future<void> addAuth(Auth auth) async {
+    final int id = (_authBox.keys.isEmpty ? 0 : _authBox.keys.last as int) + 1;
+    final newAuth = Auth(id: id, email: auth.email, password: auth.password);
+    await _authBox.put(id, newAuth);
+  }
+
+  Auth? getAuth(int id) {
+    return _authBox.get(id);
+  }
+
+  Future<void> deleteAuth(int id) async {
+    await _authBox.delete(id);
+  }
+
+  List<Auth> getAllAuths() {
+    return _authBox.values.toList();
+  }
+}
