778a125377e647ac27a61946cecd950bb1aed9e5
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-05 16:24:18 +0800

    update login_form by adding validations and actual process

--- lib/features/card_game/presentation/auth_dialog/widgets/login_form.dart
+++ lib/features/card_game/presentation/auth_dialog/widgets/login_form.dart
@@ -3,47 +3,76 @@ import 'package:aceplus/shared/utils/constant.dart';
 import 'package:aceplus/shared/utils/strings.dart';
 import 'package:aceplus/shared/widgets/solid_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../../shared/widgets/message_text.dart';
+import '../auth_bloc/auth_bloc.dart';
+import '../auth_bloc/auth_event.dart';
+import '../auth_bloc/auth_state.dart';
 
 class LoginForm extends StatelessWidget {
   LoginForm({super.key});
-  final emailController = TextEditingController();
+  final mobileNumberController = TextEditingController();
   final passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(15),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          AuthTextfield(
-            controller: emailController,
-            hintText: Str().enterEmail,
-            obscureText: false,
-          ),
-          AuthTextfield(
-            controller: passwordController,
-            hintText: Str().enterPass,
-            suffixIcon: Icon(Icons.visibility_outlined),
-            obscureText: true,
-          ),
-          SizedBox(height: 20),
-          SolidButton(
-            labelText: Str().logIn.toUpperCase(),
-            fontFamily: poppins,
-            onPressed: () => print("Logging In"),
-          ),
-          SizedBox(height: 15),
-          Text(
-            Str().fPass,
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              color: primaryWhite,
-              fontSize: 12,
-              fontFamily: poppins,
-            ),
-          ),
-        ],
+      child: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          String? message;
+          Color? messageColor;
+          if (state is AuthError) {
+            message = state.message;
+            messageColor = Colors.red;
+          } else if (state is AuthSuccess) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.of(context).pop();
+            });
+          }
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              if (message != null)
+                MessageDisplay(
+                  message: message,
+                  messageColor: messageColor!,
+                ),
+              AuthTextfield(
+                controller: mobileNumberController,
+                hintText: Str().enterMobileNumber,
+                obscureText: false,
+              ),
+              AuthTextfield(
+                controller: passwordController,
+                hintText: Str().enterPass,
+                suffixIcon: Icon(Icons.visibility_outlined),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              SolidButton(
+                labelText: Str().logIn.toUpperCase(),
+                fontFamily: poppins,
+                onPressed: () {
+                  final mobileNumber = mobileNumberController.text.trim();
+                  final password = passwordController.text.trim();
+
+                  context.read<AuthBloc>().add(SearchAuth(mobileNumber, password));
+                },
+              ),
+              SizedBox(height: 15),
+              Text(
+                Str().fPass,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: primaryWhite,
+                  fontSize: 12,
+                  fontFamily: poppins,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
