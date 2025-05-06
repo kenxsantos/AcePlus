5d2357b8ecdcefef4fac6d80059c8f4bfc5451fb
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-06 15:52:26 +0800

    update validators

--- lib/shared/utils/validators.dart
+++ lib/shared/utils/validators.dart
@@ -1,15 +1,15 @@
 String? validateMobileNumber(String mobileNumber) {
   final mobileNumberRegex = RegExp(r'^\d{11}$');
   if (!mobileNumberRegex.hasMatch(mobileNumber)) {
-    return 'Not a valid mobile number format. It must be exactly 11 digits.';
+    return 'Phone Number must be 11 digits';
   }
   return null;
 }
 
 String? validatePassword(String password) {
-  final passwordRegex = RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$');
+  final passwordRegex = RegExp(r'^.{6,}$');
   if (!passwordRegex.hasMatch(password)) {
-    return 'Password must be at least 8 characters with uppercase, lowercase, number & special char.';
+    return 'Password must be at least 6 characters';
   }
   return null;
 }
