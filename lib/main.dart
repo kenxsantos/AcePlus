ad3ed5559023553981c78bd47364f5137a713061
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-08 18:51:26 +0800

    update process on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,25 @@
-import 'package:aceplus/core/model/transaction_model/transaction_model.dart';
 import 'package:aceplus/router/router.dart';
 import 'package:aceplus/shared/utils/logged_in_checker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'core/data_source/data_source.dart';
+import 'core/data_source/auth_data_source.dart';
 import 'core/data_source/transaction_data_source.dart';
-import 'core/data_source/user_data_source.dart';
-import 'core/model/auth_model/auth_model.dart';
+import 'core/model/transaction_model/transaction_model.dart';
 import 'core/model/user_model/user_model.dart';
-import 'core/repository/aceplus_repository.dart';
+import 'core/repository/auth_repository.dart';
 import 'core/repository/transaction_repository.dart';
-import 'core/repository/user_repository.dart';
 import 'features/card_game/presentation/auth_dialog/auth_bloc/auth_bloc.dart';
 import 'features/card_game/presentation/auth_dialog/auth_bloc/auth_event.dart';
 import 'features/card_game/presentation/wallet_page/transaction_bloc/transaction_bloc.dart';
 
 Future<void> initHive() async {
   await Hive.initFlutter();
-  Hive.registerAdapter(AuthAdapter());
   Hive.registerAdapter(UserAdapter());
   Hive.registerAdapter(TransactionAdapter());
-  await Hive.openBox<Auth>('auth');
   await Hive.openBox<User>('user');
-  await Hive.openBox<Transaction>('transactions');
+  await Hive.openBox<Transaction>('transaction');
 }
 
 void main() async {
@@ -35,14 +30,11 @@ void main() async {
   final appRouter = AppRouter();
   final authDataSource = AuthDataSource();
   final authRepository = AuthRepository(authDataSource);
-  final userDataSource = UserDataSource();
-  final userRepository = UserRepository(userDataSource);
   final transactionDataSource = TransactionDataSource();
   final transactionRepository = TransactionRepository(transactionDataSource);
 
   // Temporary just to see all data in the auth box
-  final authBloc = AuthBloc(authRepository, userRepository);
-  authBloc.add(LoadAuths());
+  final authBloc = AuthBloc(authRepository);
   authBloc.add(LoadUsers());
 
   //Temporary just to check if there is an existing session
@@ -54,14 +46,14 @@ void main() async {
   runApp(
     MultiBlocProvider(
       providers: [
-        BlocProvider<AuthBloc>(create: (context) => AuthBloc(authRepository, userRepository)),
+        BlocProvider<AuthBloc>(create: (context) => AuthBloc(authRepository)),
         BlocProvider<AuthBloc>(
-          create: (context) => AuthBloc(authRepository, userRepository)..add(CheckSession()),
+          create: (context) => AuthBloc(authRepository)..add(CheckSession()),
         ),
         BlocProvider<TransactionBloc>(
           create: (context) => TransactionBloc(
             repository: transactionRepository,
-            userRepository: userRepository,
+            userRepository: authRepository,
           ),
         ),
       ],
