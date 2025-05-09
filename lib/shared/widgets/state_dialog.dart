41d14e89d9a8e45cf072358101211232b1765119
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-09 17:05:21 +0800

    add state dialog file

---
+++ lib/shared/widgets/state_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StateDialog extends StatelessWidget {
+  const StateDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
