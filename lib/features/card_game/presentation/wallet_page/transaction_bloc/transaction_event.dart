bb00cbb984b4b3704ee332604c1ab0bafb03b3aa
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-09 10:10:09 +0800

    update addtransaction and loadtransaction event

--- lib/features/card_game/presentation/wallet_page/transaction_bloc/transaction_event.dart
+++ lib/features/card_game/presentation/wallet_page/transaction_bloc/transaction_event.dart
@@ -1,15 +1,27 @@
-import '../../../../../core/model/transaction_model/transaction_model.dart';
-
 abstract class TransactionEvent {
   const TransactionEvent();
 }
 
 class LoadTotalMoney extends TransactionEvent {
-  final String userId;
+  final int userId;
   const LoadTotalMoney(this.userId);
 }
 
 class AddTransaction extends TransactionEvent {
-  final Transaction transaction;
-  const AddTransaction(this.transaction);
+  final String userId;
+  final String transactionType;
+  final String amount;
+  final String mobileNumber;
+
+  const AddTransaction({
+    required this.userId,
+    required this.transactionType,
+    required this.amount,
+    required this.mobileNumber,
+  });
+}
+
+class LoadTransaction extends TransactionEvent {
+  final int userId;
+  LoadTransaction({required this.userId});
 }
