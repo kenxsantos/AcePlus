69c08f5cf44c2390349f48f020bdf2bad2edfe2f
kenxsantos <kenxsantos@gmail.com>
2025-05-13 10:45:10 +0800

    refactor animated cards widget to use a single CardWidget for rendering and manage animations

--- lib/features/card_game/presentation/game_page/widgets/cards_widget/animated_cards.dart
+++ lib/features/card_game/presentation/game_page/widgets/cards_widget/animated_cards.dart
@@ -1,13 +1,7 @@
-import 'dart:math';
-
 import 'package:aceplus/features/card_game/presentation/game_page/widgets/cards_widget/bloc/card_bloc.dart';
-import 'package:aceplus/features/card_game/presentation/game_page/widgets/cards_widget/card_1.dart';
-import 'package:aceplus/features/card_game/presentation/game_page/widgets/cards_widget/card_2.dart';
-import 'package:aceplus/features/card_game/presentation/game_page/widgets/cards_widget/card_3.dart';
-import 'package:aceplus/features/card_game/presentation/game_page/widgets/cards_widget/card_4.dart';
+import 'package:aceplus/features/card_game/presentation/game_page/widgets/cards_widget/card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:aceplus/features/card_game/presentation/game_page/widgets/timer_widget/bloc/timer_bloc.dart';
 
 class AnimatedCards extends StatefulWidget {
   const AnimatedCards({super.key});
@@ -22,55 +16,33 @@ class _AnimatedCardsState extends State<AnimatedCards>
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<CardBloc, CardState>(
-      listenWhen: (previous, current) => current is CardIsAnimated,
       listener: (context, state) {
-        if (state is CardIsAnimated) {
-          print(
-            "Listener: Expanded - ${state.isExtended}, Flipped - ${state.isFlipped}",
-          );
+        if (state is CardGenerated) {
+          cardNumbers = state.numbers;
         }
       },
-      buildWhen: (previous, current) => current is CardIsAnimated,
+
       builder: (context, state) {
         bool isFlipped = false;
         bool isExpanded = false;
-        List<int> cardNumbers = [0, 0, 0, 0];
 
         if (state is CardIsAnimated) {
           isExpanded = state.isExtended;
           isFlipped = state.isFlipped;
-          print("Builder: Expanded - $isExpanded, Flipped - $isFlipped");
-        }
-        if (state is CardGenerated) {
-          cardNumbers = state.numbers;
-          print("Received CardGenerated state with numbers: $cardNumbers");
         }
 
         return Column(
           children: [
             Stack(
-              children: [
-                Card1(
-                  value: cardNumbers[0],
+              children: List.generate(
+                cardNumbers.length,
+                (index) => CardWidget(
+                  value: cardNumbers[index],
                   isFlipped: isFlipped,
                   isExpanded: isExpanded,
+                  index: index,
                 ),
-                Card2(
-                  value: cardNumbers[1],
-                  isFlipped: isFlipped,
-                  isExpanded: isExpanded,
-                ),
-                Card3(
-                  value: cardNumbers[2],
-                  isFlipped: isFlipped,
-                  isExpanded: isExpanded,
-                ),
-                Card4(
-                  value: cardNumbers[3],
-                  isFlipped: isFlipped,
-                  isExpanded: isExpanded,
-                ),
-              ],
+              ),
             ),
           ],
         );
---
+++ lib/features/card_game/presentation/game_page/widgets/cards_widget/card_widget.dart
@@ -0,0 +1,136 @@
+import 'dart:math';
+import 'package:aceplus/shared/utils/card_enums.dart';
+import 'package:flutter/material.dart';
+import 'package:aceplus/shared/utils/constant.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CardWidget extends StatefulWidget {
+  bool isFlipped;
+  bool isExpanded;
+  final int value;
+  final int index;
+
+  CardWidget({
+    super.key,
+    required this.isFlipped,
+    required this.isExpanded,
+    required this.value,
+    required this.index,
+  });
+
+  @override
+  State<CardWidget> createState() => _CardWidgetState();
+}
+
+class _CardWidgetState extends State<CardWidget> with TickerProviderStateMixin {
+  late AnimationController flipController;
+  late AnimationController expandController;
+  late Animation<double> flipAnimation;
+  late Animation<double> rotationAnimation;
+  late Animation<Offset> positionAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    flipController = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+
+    expandController = AnimationController(
+      duration: Duration(milliseconds: 200 + widget.index * 200),
+      vsync: this,
+    );
+
+    flipAnimation = Tween<double>(
+      begin: 0,
+      end: pi,
+    ).animate(CurvedAnimation(parent: flipController, curve: Curves.easeOut));
+
+    rotationAnimation = Tween<double>(
+      begin: 0,
+      end: 2 * pi,
+    ).animate(CurvedAnimation(parent: expandController, curve: Curves.easeOut));
+
+    positionAnimation = Tween<Offset>(
+      begin:
+          widget.isExpanded
+              ? Offset((widget.index + 1) * 0.33, 0.33)
+              : Offset.zero,
+      end:
+          widget.isExpanded
+              ? Offset.zero
+              : Offset((widget.index + 1) * 0.33, 0.33),
+    ).animate(CurvedAnimation(parent: expandController, curve: Curves.easeOut));
+  }
+
+  @override
+  void didUpdateWidget(CardWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    print(
+      "CardWidget Updated! Old Expanded: ${oldWidget.isExpanded}, New: ${widget.isExpanded}",
+    );
+    print(
+      "CardWidget Updated! Old Flipped: ${oldWidget.isFlipped}, New: ${widget.isFlipped}",
+    );
+
+    if (widget.isExpanded != oldWidget.isExpanded) {
+      print("Expand Animation Triggered: ${widget.isExpanded}");
+      widget.isExpanded
+          ? expandController.forward(from: 0)
+          : expandController.reverse();
+    }
+
+    if (widget.isFlipped != oldWidget.isFlipped) {
+      print("Flip Animation Triggered: ${widget.isFlipped}");
+      widget.isFlipped
+          ? flipController.forward(from: 0)
+          : flipController.reverse();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: Listenable.merge([flipController, expandController]),
+      builder: (context, child) {
+        final isHalfway = flipAnimation.value > pi / 2;
+        return Transform.translate(
+          offset:
+              positionAnimation.value * MediaQuery.of(context).size.width / 2,
+          child: Transform.rotate(
+            angle: rotationAnimation.value,
+            child: Transform(
+              alignment: Alignment.center,
+              transform:
+                  Matrix4.identity()
+                    ..setEntry(3, 2, 0.001)
+                    ..rotateY(flipAnimation.value),
+              child:
+                  isHalfway
+                      ? Image.asset(
+                        getCardByValue(widget.value).cardPath,
+                        width: 50,
+                        height: 66,
+                      )
+                      : Image.asset(
+                        "${cardUrl}card_back.png",
+                        width: 50,
+                        height: 66,
+                      ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    flipController.dispose();
+    expandController.dispose();
+    super.dispose();
+  }
+}
