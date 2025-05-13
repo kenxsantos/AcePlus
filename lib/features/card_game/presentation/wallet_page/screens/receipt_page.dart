51cd98593636735357703e50201891562b4f9732
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-13 17:36:00 +0800

    implement load transaction to receipt page

--- lib/features/card_game/presentation/wallet_page/screens/receipt_page.dart
+++ lib/features/card_game/presentation/wallet_page/screens/receipt_page.dart
@@ -2,11 +2,35 @@ import 'package:aceplus/features/card_game/presentation/wallet_page/widgets/rece
 import 'package:aceplus/shared/utils/constant.dart';
 import 'package:aceplus/shared/utils/strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 
-class ReceiptPage extends StatelessWidget {
+import '../transaction_bloc/transaction_bloc.dart';
+import '../transaction_bloc/transaction_event.dart';
+import '../transaction_bloc/transaction_state.dart';
+
+class ReceiptPage extends StatefulWidget {
   final String id;
-  const ReceiptPage({super.key, required this.id});
+  final String transactionId;
+
+  const ReceiptPage({super.key, required this.id, required this.transactionId});
+
+  @override
+  State<ReceiptPage> createState() => _ReceiptPageState();
+}
+
+class _ReceiptPageState extends State<ReceiptPage> {
+  late final String transactionId;
+
+  @override
+  void initState() {
+    super.initState();
+    transactionId = widget.transactionId;
+    context.read<TransactionBloc>().add(
+      LoadTransaction(transactionId: int.parse(transactionId)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,55 +52,129 @@ class ReceiptPage extends StatelessWidget {
         backgroundColor: primaryBlack,
         automaticallyImplyLeading: true,
         leading: IconButton(
-          onPressed: () => context.go('/transaction-history/$id'),
+          onPressed: () => context.go('/transaction-history/${widget.id}'),
           color: primaryWhite,
           icon: Icon(Icons.arrow_back_ios_new_rounded),
         ),
       ),
-      body: Container(
-        alignment: Alignment.topCenter,
-        color: primaryBlack,
-        padding: EdgeInsets.all(25),
-        child: Container(
-          padding: EdgeInsets.all(25),
-          width: 500,
-          height: 350,
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 22, 22, 22),
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.asset('${iconUrl}receipt_icon.png'),
-              Divider(height: 5),
-              Column(
-                children: [
-                  ReceiptInfo(
-                    txtTitle: "Amount",
-                    txtValue: "-50.00",
-                    txtColor: Colors.red,
-                  ),
-                  ReceiptInfo(
-                    txtTitle: "Date",
-                    txtValue: "April 1, 2025",
-                    txtColor: primaryWhite,
-                  ),
-                  ReceiptInfo(
-                    txtTitle: "Time",
-                    txtValue: "9:45 AM",
-                    txtColor: primaryWhite,
-                  ),
-                  ReceiptInfo(
-                    txtTitle: "Reference No.",
-                    txtValue: "213513203461348",
-                    txtColor: primaryWhite,
-                  ),
-                ],
+      body: BlocBuilder<TransactionBloc, TransactionState>(
+        builder: (context, state) {
+          if (state is TransactionLoading) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is TransactionLoaded) {
+            final transaction = state.transaction;
+            return Container(
+              alignment: Alignment.topCenter,
+              color: primaryBlack,
+              padding: EdgeInsets.all(25),
+              child: Container(
+                padding: EdgeInsets.all(25),
+                width: 500,
+                height: 350,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 22, 22, 22),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset('${iconUrl}receipt_icon.png'),
+                    Divider(height: 5),
+                    Column(
+                      children: [
+                        ReceiptInfo(
+                          txtTitle: "Amount",
+                          txtValue: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: transaction.transactionType == "Cash In" ? "+" : "-",
+                                style: TextStyle(
+                                  fontFamily: lemon,
+                                  color: transaction.transactionType == "Cash In" ? Color(0xFF42F271) : Color(0xFFF24242),
+                                ),
+                              ),
+                              TextSpan(
+                                text: '\u20B1',
+                                style: TextStyle(
+                                  fontFamily: 'Arial',
+                                  fontSize: 15,
+                                  color: transaction.transactionType == "Cash In" ? Color(0xFF42F271) : Color(0xFFF24242),
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' ${NumberFormat('#,##0.00').format(double.parse(transaction.amount.toString()))}',
+                                style: TextStyle(
+                                  fontFamily: lemon,
+                                  color: transaction.transactionType == "Cash In" ? Color(0xFF42F271) : Color(0xFFF24242),
+                                ),
+                              ),
+                            ],
+                          ),
+                          txtColor: transaction.transactionType == "Cash In" ? Color(0xFF42F271) : Color(0xFFF24242),
+                        ),
+                        ReceiptInfo(
+                          txtTitle: "Date",
+                          txtValue: TextSpan(
+                            text: DateFormat('MMMM dd, yyyy').format(transaction.transactionDate),
+                            style: TextStyle(
+                              fontFamily: lemon,
+                              color: primaryWhite,
+                            ),
+                          ),
+                          txtColor: primaryWhite,
+                        ),
+                        ReceiptInfo(
+                          txtTitle: "Time",
+                          txtValue: TextSpan(
+                            text: DateFormat('hh:mm a').format(transaction.transactionDate),
+                            style: TextStyle(
+                              fontFamily: lemon,
+                              color: primaryWhite,
+                            ),
+                          ),
+                          txtColor: primaryWhite,
+                        ),
+                        ReceiptInfo(
+                          txtTitle: "Mobile Number Used",
+                          txtValue: TextSpan(
+                            text: transaction.mobileNumber,
+                            style: TextStyle(
+                              fontFamily: lemon,
+                              color: primaryWhite,
+                            ),
+                          ),
+                          txtColor: primaryWhite,
+                        ),
+                        ReceiptInfo(
+                          txtTitle: "Reference No.",
+                          txtValue: TextSpan(
+                            text: transaction.referenceNumber,
+                            style: TextStyle(
+                              fontFamily: lemon,
+                              color: primaryWhite,
+                            ),
+                          ),
+                          txtColor: primaryWhite,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
+            );
+          } else if (state is TransactionError) {
+            return Center(
+              child: Text(state.error, style: TextStyle(color: Colors.red)),
+            );
+          } else {
+            return Center(
+              child: Text(
+                "No data available",
+                style: TextStyle(color: primaryWhite),
+              ),
+            );
+          }
+        },
       ),
     );
   }
