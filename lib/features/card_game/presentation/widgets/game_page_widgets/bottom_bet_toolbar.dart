73632f9a83f3ea10fe4dbe4c693822d2ec215775
Ken Santos <kenxsantos@gmail.com>
2025-05-20 18:36:57 +0800

    refactor: remove onTap callback from BottomToolbarWidget and update visibility logic in BottomBetWidget

--- lib/features/card_game/presentation/widgets/game_page_widgets/bottom_bet_toolbar.dart
+++ lib/features/card_game/presentation/widgets/game_page_widgets/bottom_bet_toolbar.dart
@@ -2,95 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class BottomToolbarWidget extends StatelessWidget {
-  final VoidCallback onTap;
-
-  const BottomToolbarWidget({super.key, required this.onTap});
+  const BottomToolbarWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
 
-    return GestureDetector(
-      onTap: onTap,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Image.asset(
-            'assets/images/bottom_placing_bet.png',
-            width: screenWidth,
-            height: screenHeight * 0.08,
-            fit: BoxFit.fill,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
-            child: SizedBox(
-              width: screenWidth * 0.9,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Stack(
-                    children: [
-                      Text(
-                        'Place your bets !',
-                        style: GoogleFonts.lemon(
-                          textStyle: TextStyle(
-                            fontSize: screenHeight * 0.018,
-                            fontWeight: FontWeight.bold,
-                            foreground:
-                                Paint()
-                                  ..style = PaintingStyle.stroke
-                                  ..strokeWidth = 1.5
-                                  ..color = const Color(0xFF7D1F22),
-                          ),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.asset(
+          'assets/images/bottom_placing_bet.png',
+          width: screenWidth,
+          height: screenHeight * 0.08,
+          fit: BoxFit.fill,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+          child: SizedBox(
+            width: screenWidth * 0.9,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Stack(
+                  children: [
+                    Text(
+                      'Place your bets !',
+                      style: GoogleFonts.lemon(
+                        textStyle: TextStyle(
+                          fontSize: screenHeight * 0.018,
+                          fontWeight: FontWeight.bold,
+                          foreground:
+                              Paint()
+                                ..style = PaintingStyle.stroke
+                                ..strokeWidth = 1.5
+                                ..color = const Color(0xFF7D1F22),
                         ),
                       ),
-                      Text(
-                        'Place your bets !',
-                        style: GoogleFonts.lemon(
-                          textStyle: TextStyle(
-                            fontSize: screenHeight * 0.018,
-                            fontWeight: FontWeight.bold,
-                            color: const Color(0xFFFFD700),
-                          ),
+                    ),
+                    Text(
+                      'Place your bets !',
+                      style: GoogleFonts.lemon(
+                        textStyle: TextStyle(
+                          fontSize: screenHeight * 0.018,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xFFFFD700),
                         ),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: screenHeight * 0.04,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Add cancel button functionality here
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFFFD700),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                          side: const BorderSide(
-                            color: Color(0xFF7D1F22),
-                            width: 1.5,
-                          ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: screenHeight * 0.04,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Add cancel button functionality here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFFFD700),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        side: const BorderSide(
+                          color: Color(0xFF7D1F22),
+                          width: 1.5,
                         ),
                       ),
-                      child: Text(
-                        'Cancel',
-                        style: GoogleFonts.lemon(
-                          textStyle: TextStyle(
-                            fontSize: screenHeight * 0.015,
-                            fontWeight: FontWeight.bold,
-                          ),
+                    ),
+                    child: Text(
+                      'Cancel',
+                      style: GoogleFonts.lemon(
+                        textStyle: TextStyle(
+                          fontSize: screenHeight * 0.015,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
--- lib/features/card_game/presentation/widgets/game_page_widgets/bottom_widget.dart
+++ lib/features/card_game/presentation/widgets/game_page_widgets/bottom_widget.dart
@@ -1,16 +1,11 @@
+import 'package:aceplus/features/card_game/presentation/bloc/card_bloc/card_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bottom_bet_container.dart';
 import 'bottom_bet_toolbar.dart';
 
 class BottomBetWidget extends StatelessWidget {
-  final ValueNotifier<bool> _isVisible = ValueNotifier<bool>(false);
-
-  BottomBetWidget({super.key});
-
-  void _toggleVisibility() {
-    _isVisible.value = !_isVisible.value;
-  }
-
+  const BottomBetWidget({super.key});
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
@@ -18,25 +13,24 @@ class BottomBetWidget extends StatelessWidget {
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
-        ValueListenableBuilder<bool>(
-          valueListenable: _isVisible,
-          builder: (context, isVisible, child) {
+        BlocBuilder<CardBloc, CardState>(
+          builder: (context, state) {
             return AnimatedPositioned(
               duration: const Duration(milliseconds: 600),
               curve: Curves.easeInOut,
-              bottom: isVisible ? 0 : -screenHeight * 0.23,
+              bottom: state.bets.isNotEmpty ? 0 : -screenHeight * 0.23,
               child: BottomBetContainer(),
             );
           },
         ),
-        ValueListenableBuilder<bool>(
-          valueListenable: _isVisible,
-          builder: (context, isVisible, child) {
+
+        BlocBuilder<CardBloc, CardState>(
+          builder: (context, state) {
             return AnimatedPositioned(
               duration: const Duration(milliseconds: 600),
               curve: Curves.easeInOut,
-              bottom: isVisible ? screenHeight * 0.23 - 2 : 0,
-              child: BottomToolbarWidget(onTap: _toggleVisibility),
+              bottom: state.bets.isNotEmpty ? screenHeight * 0.23 - 2 : 0,
+              child: BottomToolbarWidget(),
             );
           },
         ),
