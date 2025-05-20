ea5d825269fa495a396c057a3d771f4db5b8bdb0
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-20 17:00:15 +0800

    update data transaction repository

--- lib/features/card_game/data/repositories/transaction_repository.dart
+++ lib/features/card_game/data/repositories/transaction_repository.dart
@@ -1,30 +1,68 @@
 import 'package:aceplus/features/card_game/data/model/transaction_model/transaction_model.dart';
 import 'package:aceplus/features/card_game/data/datasource/transaction_data_source.dart';
+import 'package:aceplus/features/card_game/domain/entities/transaction_entity.dart';
+import '../../domain/repositories/transaction_repository.dart';
 
-class TransactionRepository {
+class TransactionRepositoryImpl implements TransactionRepository {
   final TransactionDataSource _dataSource;
 
-  TransactionRepository(this._dataSource);
+  TransactionRepositoryImpl(this._dataSource);
 
-  Future<void> addTransaction(Transaction transaction) async {
-    await _dataSource.addTransaction(transaction);
+  Transaction _toTransactionModel(TransactionEntity entity) {
+    return Transaction(
+      transactionId: entity.transactionId,
+      userId: entity.userId,
+      transactionType: entity.transactionType,
+      amount: entity.amount,
+      mobileNumber: entity.mobileNumber,
+      transactionDate: entity.transactionDate,
+      referenceNumber: entity.referenceNumber,
+    );
   }
 
-  Transaction? getTransaction(int transactionId) {
-    return _dataSource.getTransaction(transactionId);
+  TransactionEntity _toTransactionEntity(Transaction model) {
+    return TransactionEntity(
+      transactionId: model.transactionId,
+      userId: model.userId,
+      transactionType: model.transactionType,
+      amount: model.amount,
+      mobileNumber: model.mobileNumber,
+      transactionDate: model.transactionDate,
+      referenceNumber: model.referenceNumber,
+    );
   }
 
-  List<Transaction> getTransactionsByUserId(int userId) {
-    return _dataSource.getTransactionsByUserId(userId);
+  @override
+  Future<void> addTransaction(TransactionEntity transactionEntity) async {
+    final transactionModel = _toTransactionModel(transactionEntity);
+    await _dataSource.addTransaction(transactionModel);
   }
 
-  Future<List<Transaction>> getTransactionsByUserIdAndType(
+  @override
+  TransactionEntity? getTransaction(int transactionId) {
+    final transactionModel = _dataSource.getTransaction(transactionId);
+    return transactionModel != null
+        ? _toTransactionEntity(transactionModel)
+        : null;
+  }
+
+  @override
+  List<TransactionEntity> getTransactionsByUserId(int userId) {
+    return _dataSource
+        .getTransactionsByUserId(userId)
+        .map(_toTransactionEntity)
+        .toList();
+  }
+
+  @override
+  Future<List<TransactionEntity>> getTransactionsByUserIdAndType(
     int userId,
     String transactionType,
   ) async {
-    return await _dataSource.getTransactionsByUserIdAndType(
+    final models = await _dataSource.getTransactionsByUserIdAndType(
       userId,
       transactionType,
     );
+    return models.map(_toTransactionEntity).toList();
   }
 }
