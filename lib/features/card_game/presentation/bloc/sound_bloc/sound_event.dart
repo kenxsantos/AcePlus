f8324dbc9f4f9c3faf2ae091c4f7545035af6536
Ken Santos <kenxsantos@gmail.com>
2025-05-20 09:56:38 +0800

    Revert "Clean architecture"

--- lib/features/card_game/presentation/bloc/sound_bloc/sound_event.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class SoundEvent extends Equatable {
-  @override
-  List<Object?> get props => [];
-}
-
-class ToggleSound extends SoundEvent {}
-class LoadSoundState extends SoundEvent {}
--- lib/features/card_game/presentation/bloc/sound_bloc/sound_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class SoundState extends Equatable {
-  @override
-  List<Object?> get props => [];
-}
-
-class SoundPlaying extends SoundState {}
-class SoundMuted extends SoundState {}
