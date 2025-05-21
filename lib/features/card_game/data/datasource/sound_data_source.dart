3aad0fcda0c2295019723976c6f4dc63be6bcdf6
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-21 17:07:32 +0800

    update sound data source: add function for background audio slider

--- lib/features/card_game/data/datasource/sound_data_source.dart
+++ lib/features/card_game/data/datasource/sound_data_source.dart
@@ -3,15 +3,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SoundDataSource {
   final AudioPlayer _player = AudioPlayer();
+  static const _isPlaying = 'isPlaying';
+  static const _volumeKey = 'bgmVolume';
 
   Future<bool> getSoundState() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('isPlaying') ?? true;
+    return prefs.getBool(_isPlaying) ?? true;
   }
 
   Future<void> saveSoundState(bool isPlaying) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isPlaying', isPlaying);
+    await prefs.setBool(_isPlaying, isPlaying);
   }
 
   Future<void> setupBackgroundAudio() async {
@@ -32,5 +34,18 @@ class SoundDataSource {
 
   Future<void> pauseBackgroundAudio() async {
     await _player.pause();
+  }
+
+  Future<void> setBackgroundVolume(double volume) async {
+    final normalizedVolume = (volume / 100).clamp(0.0, 1.0);
+    await _player.setVolume(normalizedVolume);
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble(_volumeKey, normalizedVolume);
+  }
+
+  Future<double> getBackgroundVolume() async {
+    final prefs = await SharedPreferences.getInstance();
+    return (prefs.getDouble(_volumeKey) ?? 1.0) * 100;
   }
 }
