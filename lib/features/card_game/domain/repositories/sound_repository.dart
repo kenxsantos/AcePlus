dcb361b99938a550dbe7ab7172856976bf7db0ff
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-21 10:34:45 +0800

    update domain sound repository

--- lib/features/card_game/domain/repositories/sound_repository.dart
+++ lib/features/card_game/domain/repositories/sound_repository.dart
@@ -1,4 +1,7 @@
 abstract class SoundRepository {
   Future<bool> getSoundState();
   Future<void> saveSoundState(bool isPlaying);
+  Future<void> setupBackgroundAudio();
+  Future<void> pauseBackgroundAudio();
+  Future<void> resumeBackgroundAudio();
 }
