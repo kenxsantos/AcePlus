60d7a76049f49ebdab5ec7d48df2a801c26101cf
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-21 10:36:08 +0800

    update data sound repository

--- lib/features/card_game/data/repositories/sound_repository.dart
+++ lib/features/card_game/data/repositories/sound_repository.dart
@@ -1,12 +1,25 @@
 import 'package:aceplus/features/card_game/data/datasource/sound_data_source.dart';
+import 'package:aceplus/features/card_game/domain/repositories/sound_repository.dart';
 
-class SoundRepository {
+class SoundRepositoryImpl implements SoundRepository {
   final SoundDataSource soundDataSource;
 
-  SoundRepository({required this.soundDataSource});
+  SoundRepositoryImpl({required this.soundDataSource});
 
+  @override
   Future<bool> getSoundState() => soundDataSource.getSoundState();
 
+  @override
   Future<void> saveSoundState(bool isPlaying) =>
       soundDataSource.saveSoundState(isPlaying);
+
+  @override
+  Future<void> setupBackgroundAudio() => soundDataSource.setupBackgroundAudio();
+
+  @override
+  Future<void> resumeBackgroundAudio() =>
+      soundDataSource.resumeBackgroundAudio();
+
+  @override
+  Future<void> pauseBackgroundAudio() => soundDataSource.pauseBackgroundAudio();
 }
