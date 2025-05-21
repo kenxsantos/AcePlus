c5b7c6f3b96195e6e8fe884e9408e7c64e8a4dd5
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-05-21 10:35:22 +0800

    apply setup audio in data source

--- lib/features/card_game/data/datasource/sound_data_source.dart
+++ lib/features/card_game/data/datasource/sound_data_source.dart
@@ -1,6 +1,9 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SoundDataSource {
+  final AudioPlayer _player = AudioPlayer();
+
   Future<bool> getSoundState() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getBool('isPlaying') ?? true;
@@ -9,5 +12,18 @@ class SoundDataSource {
   Future<void> saveSoundState(bool isPlaying) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isPlaying', isPlaying);
+  }
+
+  Future<void> setupBackgroundAudio() async {
+    await _player.setReleaseMode(ReleaseMode.loop);
+    await _player.setSource(AssetSource('sounds/jazz_music_bg.MP3'));
+  }
+
+  Future<void> resumeBackgroundAudio() async {
+    await _player.resume();
+  }
+
+  Future<void> pauseBackgroundAudio() async {
+    await _player.pause();
   }
 }
